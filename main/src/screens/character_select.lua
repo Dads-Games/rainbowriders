1f782c2a120625342d3df4c67ed568fcd516589d
jonrick <jonrick@users.noreply.github.com>
2024-07-29 22:51:41 +0000

    deploy: d979df084e8f783db31d9afa3cd2a6196c7cff70

--- main/src/screens/character_select.lua
+++ main/src/screens/character_select.lua
@@ -1,40 +1,47 @@
 function CharacterSelect(state)
     local player = state.player
-    local selected_char = 1
-    local char_names = {
-        "lizzie", "lily", "riker", "lukas", "maverick", "michael"
-    }
 
     function character_select_controller()
+        local selected_rider_index = state.player.rider
+        local next_rider_index = min(#RIDERS, selected_rider_index + 1)
+        local prev_rider_index = max(1, selected_rider_index - 1)
+
+        -- button: left
         if btnp(0) then
-            selected_char = max(1, selected_char - 1)
+            state.player.rider = prev_rider_index
             sfx(60) -- character switching sfx
+
+        -- button: right
         elseif btnp(1) then
-            selected_char = min(#char_names, selected_char + 1)
+            state.player.rider = next_rider_index
             sfx(60) -- character switching sfx
         end
 
         -- Confirm selection and start game
-        if btnp(4) then
-            player.sprite = selected_char
+        if btnp(4) or btnp (5) then
             state.screen = GetReady(state)
             sfx(59) -- character selected sfx
         end
     end
 
     function character_select_view()
-        print("select your character", 20, 20, 7)
-        for i = 1, 6 do
+
+        local selected_rider_index = state.player.rider
+
+        printc("select your character", 20, 20, 7)
+        for i = 1, #RIDERS do
+
             local x = 5 + (i - 1) * 22
             local y = 40
-            if i == selected_char then
+            if i == selected_rider_index then
                 rect(x - 2, y - 2, x + 10, y + 10, 7) -- Highlight selected character
             end
-            spr(i, x, y)
+            spr(RIDERS[i].sprite, x, y)
         end
-        print(char_names[selected_char], 50, 65, 7) -- Display character name
-        print("press left or right", 20, 90, 7)
-        print("then start to confirm", 30, 100, 7)
+        printc(RIDERS[selected_rider_index].name, 50, 65, 7) -- Display character name
+        printc("press left or right", 20, 90, 7)
+        printc("then start to confirm", 30, 100, 7)
+
     end
 
     return {
